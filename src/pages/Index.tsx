eea108077fd3956c818fc44b465b5589c4ea6acc
ricoa21 <rikspresso@gmail.com>
2026-04-23 20:17:17 +0100

    scroll

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,23 +1,25 @@
 import Hero from "@/components/Hero";
-import Problem from "@/components/Problem";  
-import Audience from "@/components/Audience";
-import HowItWorks from "@/components/HowItWorks";
-import ServicesComparison from "@/components/ServicesComparison";
-import FAQ from "@/components/FAQ";
-import Footer from "@/components/Footer";
+  import Problem from "@/components/Problem";  
+  import Audience from "@/components/Audience";
+  import HowItWorks from "@/components/HowItWorks";
+  import ServicesComparison from "@/components/ServicesComparison";
+  import FAQ from "@/components/FAQ";
+  import Footer from "@/components/Footer";
+  import ScrollToTop from "@/components/ScrollToTop";
 
-const Index = () => {
-  return (
-    <div className="min-h-screen">
-      <Hero />
-      <HowItWorks />
-      <Audience />
-      <Problem />
-      <ServicesComparison />
-      <FAQ />
-      <Footer />
-    </div>
-  );
-};
+  const Index = () => {
+    return (
+      <div className="min-h-screen">
+        <Hero />
+        <HowItWorks />
+        <Audience />
+        <Problem />
+        <ServicesComparison />
+        <FAQ />
+        <ScrollToTop />
+        <Footer />
+      </div>
+    );
+  };
 
-export default Index;
+  export default Index;
