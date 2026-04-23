57a718e0de66faaa4c128d5207d13fce4c72160c
ricoa21 <rikspresso@gmail.com>
2026-04-23 20:19:45 +0100

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+// @ts-ignore - QueryClient exists in v5 but github.dev linter misreports
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
